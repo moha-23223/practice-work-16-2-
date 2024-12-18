f922dfa1a63577fa00ea6a83dd3f46b4f0fc7038
MOHANAD KHALEEL <150276506+moha-23223@users.noreply.github.com>
2024-12-18 21:16:55 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function greeting() {
+    const username = prompt("Введите имя пользователя");
+    if (!username) {
+        throw new Error("Имя обязательно для заполнения");
+    }
+}
+
+try {
+    greeting();
+} catch (error) {
+    alert(error.message);
+}
